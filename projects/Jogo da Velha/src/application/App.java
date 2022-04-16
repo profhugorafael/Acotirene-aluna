0ff5fb5a8d12ebc56b87cd10041cb494468122e5
Hugo Rafael <hugorafell@gmail.com>
2022-04-16 18:04:08 -0300

    Update aula 16/04/22

--- projects/Jogo da Velha/src/application/App.java
+++ projects/Jogo da Velha/src/application/App.java
@@ -16,13 +16,29 @@ public class App {
 
     jogo.play(1, 1, "X");
     System.out.println(jogo);
+    System.out.println(jogo.verify());
 
-    jogo.play(2, 2, "O");
+    jogo.play(2, 2, "0");
     System.out.println(jogo);
+    System.out.println(jogo.verify());
     
-    jogo.play(3, 3, "X");
+    jogo.play(2, 1, "X");
     System.out.println(jogo);
+    System.out.println(jogo.verify());
 
+    jogo.play(2, 3, "X");
+    System.out.println(jogo);
+    System.out.println(jogo.verify());
+
+    jogo.play(1, 3, "0");
+    System.out.println(jogo);
+    System.out.println(jogo.verify());
+    
+    jogo.play(3, 1, "0");
+    System.out.println(jogo);
+    System.out.println(jogo.verify());
+    
+  
     System.out.println( jogo.showScores() );
 
   }
--- projects/Jogo da Velha/src/entities/Table.java
+++ projects/Jogo da Velha/src/entities/Table.java
@@ -17,7 +17,10 @@ public class Table {
     table = new String[dimension][dimension];
   }
   
-  // métodos
+  // ----------------------------------------
+  // play methods
+  // play, reset, match
+
   public boolean play(int linha, int coluna, String attemp){
     linha--;
     coluna--;
@@ -40,9 +43,118 @@ public class Table {
     winner = null;
   }
 
-  public void verify(){
+  public void match(){
+    if(winner == "X") player1.win();
+    else if(winner == "0") player2.win();
+    else System.out.println("No winner!");
+  }
+
+  // ----------------------------------------
+  // verify methods
+  // to find the winner
+
+  public boolean verify(){
+
+    if(table[0][0] == null) return false;
+
+    // veryfing main diagonal
+    winner = verifyMainDiagonal();
+    if(winner != null) return true;
+
+    winner = verifySecundaryDiagonal();
+    if(winner != null) return true;
+
+    winner = verifyColumns();
+    if(winner != null ) return true;
+
+    winner = verifyLines();
+    if(winner != null) return true;
+
+    if( isFull() ){
+      winner = "-";
+      match();
+      return true;
+    } return false;
 
   }
+
+  // ----------------------------------------
+  // testing methods
+  // dont modify
+
+  private boolean isFull(){
+    for(int i=0; i<dimension; i++){
+      for(int j=0; j<dimension; j++){
+        if(table[i][j] == null) 
+          return false;
+      }
+    }
+
+    return true;
+  }
+
+  private String verifyColumns() {
+    String hasWinner = null;
+
+    for(int i=0; i<dimension; i++){
+      String first = table[0][i];
+      hasWinner = first; // reset
+
+      for(int j=0; j<dimension; j++){
+        if(table[j][i] == null || table[j][i] != first)
+          hasWinner = null;
+      }
+
+      if(hasWinner != null) break; // already has a winner
+    }
+
+    return hasWinner;
+  }
+
+  private String verifyLines() {
+    String hasWinner = null;
+
+    for(int i=0; i<dimension; i++){
+      String first = table[i][0];
+      hasWinner = first; // reset
+
+      for(int j=0; j<dimension; j++){
+        if(table[i][j] == null || table[i][j] != first)
+          hasWinner = null;
+      }
+
+      if(hasWinner != null) break; // already has a winner
+    }
+
+    return hasWinner;
+  }
+
+  private String verifySecundaryDiagonal() {
+    String first = table[0][dimension-1];
+    String hasWinner = first;
+
+    for(int i=1; i<dimension; i++){
+      if(table[i][dimension-i-1] == null || table[i][dimension-i-1] != first)
+        hasWinner = null;
+    }
+
+    return hasWinner;
+  }
+
+  private String verifyMainDiagonal(){
+    String first = table[0][0];
+    String hasWinner = first;
+
+    for(int i=1; i<dimension; i++){
+      if(table[i][i] == null || table[i][i] != first)
+        hasWinner = null;
+    }
+
+    return hasWinner;
+  }
+
+  // ----------------------------------------
+  // printing methods
 
   public String showScores(){
     String aux = "------------------------\n";
@@ -84,8 +196,3 @@ public class Table {
   
 
 }
-
-// [0][0] [0][1] [0][2] 
-// [1][0] [1][1] [1][2]
-// [2][0] [2][1] [2][2]
-
